a929c1314ccfc33fc269a716a2075c922c980336
Zura014 <zuragagnidze50@gmail.com>
2025-06-14 16:00:57 +0400

    feat: add audio recorder component with initial structure

---
+++ src/app/shared/components/audio-recorder/audio-recorder.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-audio-recorder',
+  templateUrl: './audio-recorder.component.html',
+})
+export class AudioRecorderComponent {}
