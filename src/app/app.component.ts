50f536c66e97abc34f7de33c7eeb42d3e15f98d5
Zura014 <zuragagnidze50@gmail.com>
2025-06-15 17:25:57 +0400

    working on

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/layout/components/header.component';
+import { AudioRecorderComponent } from "./shared/components/audio-recorder/audio-recorder.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, AudioRecorderComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {}
