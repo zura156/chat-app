668a2dc1e9757218665e473383dabff19ba80724
Zura014 <zuragagnidze50@gmail.com>
2025-07-07 09:47:54 +0400

    test

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { inject } from '@vercel/analytics';
-import 'hammerjs';
 
 inject({ debug: false });
 
