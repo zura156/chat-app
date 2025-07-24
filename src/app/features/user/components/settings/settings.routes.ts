430e48c3dccb0450fd8d66e548c2df9fe94c8dcc
Zura014 <zuragagnidze50@gmail.com>
2025-07-24 19:05:10 +0400

    feat: define routes for user settings component

--- src/app/features/user/components/settings/settings.routes.ts
+++ src/app/features/user/components/settings/settings.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { UserSettingsComponent } from './layout/user-settings.component';
 
-export const settingsRoutes: Routes = [];
+export const settingsRoutes: Routes = [
+  {
+    path: '',
+    component: UserSettingsComponent,
+  },
+];
