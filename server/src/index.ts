66a8e7b41597cda64caa70329a7237288570a6aa
zura156 <zuragagnidze50@gmail.com>
2025-08-22 00:28:03 +0400

    removed unwanted cors options

--- server/src/index.ts
+++ server/src/index.ts
@@ -40,16 +40,6 @@ app.use(
   })
 );
 
-app.options(
-  '*',
-  cors({
-    origin: config.clientUrl,
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'X-CSRF-Token'],
-  })
-);
-
 setupWebSocket(server);
 
 // ---------------------------------------------
