60383c333372cfb73c594503364606b89aab6301
zura156 <zuragagnidze50@gmail.com>
2025-09-04 21:20:15 +0400

    added dynamic icon

--- src/app/features/messages/start/messages-start.compoent.ts
+++ src/app/features/messages/start/messages-start.compoent.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { HlmButton } from '@spartan-ng/helm/button';
+import { ThemeService } from '../../../shared/services/theme.service';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-messages-start',
   imports: [RouterLink, HlmButton],
   templateUrl: './messages-start.compoent.html',
 })
-export class MessagesStartComponent {}
+export class MessagesStartComponent implements OnInit {
+  private readonly destroyRef = inject(DestroyRef);
+  private readonly themeService = inject(ThemeService);
+  isDarkMode = signal<boolean>(false);
+
+  ngOnInit(): void {
+    this.themeService.themeMode$
+      .pipe(
+        takeUntilDestroyed(this.destroyRef),
+        tap((mode) => this.isDarkMode.set(mode === 'dark'))
+      )
+      .subscribe();
+  }
+}
