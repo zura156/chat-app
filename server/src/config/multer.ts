f7566fedce5b48088e6254d761c90aeb4544da05
Zura014 <zuragagnidze50@gmail.com>
2025-09-08 20:15:29 +0400

    added utf-8 suport for file naming

--- server/src/config/multer.ts
+++ server/src/config/multer.ts
@@ -6,15 +6,22 @@ const storage = multer.diskStorage({
     cb(null, 'uploads/');
   },
   filename: (req, file, cb) => {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-    cb(
-      null,
-      file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname)
+    // Ensure proper UTF-8 handling
+    const originalName = Buffer.from(file.originalname, 'latin1').toString(
+      'utf8'
     );
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+    cb(null, uniqueSuffix + '-' + originalName);
   },
 });
 
 export const uploadMiddleware = multer({
   limits: { fileSize: 30 * 1024 * 1024 },
   storage,
+  fileFilter: (req, file, cb) => {
+    file.originalname = Buffer.from(file.originalname, 'latin1').toString(
+      'utf8'
+    );
+    cb(null, true);
+  },
 });
