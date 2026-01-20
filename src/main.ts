0349b7ed9a247a512f09b78351ff4b2166835d44
Zura156 <zuragagnidze50@gmail.com>
2026-01-20 23:36:40 +0400

    removed vercel

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { inject } from '@vercel/analytics';
 import 'hammerjs';
 
-inject({ debug: false });
-
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
-  console.error(err)
+  console.error(err),
 );
