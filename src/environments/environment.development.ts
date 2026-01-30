e2a6492a5f83c21f1564892acb685e4823a8a827
Zura156 <zuragagnidze50@gmail.com>
2026-01-30 23:06:13 +0400

    envsss

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,4 +2,5 @@ export const environment = {
   production: false,
   apiUrl: 'http://localhost:3000',
   wsUrl: 'ws://localhost:3000',
+  recaptchaSiteKey: '6LczclssAAAAALw8rGYCj6RRYCOrcZ8XIDy7X_tm',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,4 +2,5 @@ export const environment = {
   production: true,
   apiUrl: 'https://api-chat-app.zura156.xyz',
   wsUrl: 'wss://api-chat-app.zura156.xyz',
+  recaptchaSiteKey: '6LczclssAAAAALw8rGYCj6RRYCOrcZ8XIDy7X_tm',
 };
